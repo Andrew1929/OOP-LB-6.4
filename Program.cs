4b134fd641d6181e9bdb796b11a9156ff0e40479
Andriy Rachenko <108069918+Andrew1929@users.noreply.github.com>
2022-11-22 18:55:19 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace lab6_4
+{
+    public class MyFactory<T> where T : new()
+    {
+        public static T FacrotyMethod()
+        {
+            return new T();
+        }
+    }
+}
+
